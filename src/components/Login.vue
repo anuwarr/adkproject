5444322347112db3c6441e2a6c186b80a9ec23cb
Ahmed Anwar <anwar@bml.com.mv>
2022-09-20 15:12:17 +0500

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,44 @@
+<template>
+
+<section>
+    <div class="ui segment grey login-form">
+        <h2 class="ui header">Station Login</h2>
+        <form class="ui form">
+            <div class="ui stacked segment">
+                <div class="field">
+                    <div class="ui left icon input big">
+                        <i class="user icon"></i>
+                        <input type="text" placeholder="User Name" />
+                    </div>
+
+                </div>
+                <div class="field">
+                    <div class="ui left icon input big">
+                        <i class="lock icon"></i>
+                        <input type="password" placeholder="Password"/>
+                    </div>
+
+                </div>
+                <button class="ui button big blue fluid " >Login</button>
+
+            </div>
+        </form>
+    </div>
+    
+</section>
+    
+</template>
+
+<style scoped>
+section{
+    height: 100vh;
+    background-color: #ececec;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.login-form{
+    width: 450px;
+}
+
+</style>
